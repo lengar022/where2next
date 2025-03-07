efb8e973320d465f5db958c2e4ab71a6a8af07ac
Garrett Lenihan <garrettleihan@gmail.com>
2025-03-07 20:04:49 +0000

    User Model Tests passing for both MemStore and JsonStore

--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,6 @@
-// import { userMemStore } from "./mem/user-mem-store.ts";
-// import { categoryMemStore } from "./mem/category-mem-store.ts";
-// import { placemarkMemStore } from "./mem/placemark-mem-store.ts";
-
+import { userMemStore } from "./mem/user-mem-store.js";
+import { categoryMemStore } from "./mem/category-mem-store.js";
+import { placemarkMemStore } from "./mem/placemark-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
 import { categoryJsonStore } from "./json/category-json-store.js";
 import { placemarkJsonStore } from "./json/placemark-json-store.js";
@@ -11,9 +10,17 @@ export const db = {
   categoryStore: null,
   placemarkStore: null,
 
-  init() {
-    this.userStore = userJsonStore;
-    this.categoryStore = categoryJsonStore;
-    this.placemarkStore = placemarkJsonStore;
+  init(storeType) {
+    switch (storeType) {
+      case "json":
+        this.userStore = userJsonStore;
+        this.categoryStore = categoryJsonStore;
+        this.placemarkStore = placemarkJsonStore;
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.categoryStore = categoryMemStore;
+        this.placemarkStore = placemarkMemStore;
+    }
   },
 };
