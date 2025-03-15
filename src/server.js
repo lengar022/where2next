6b04fed0744f2388b6876e6e44ed03600ae8ea40
Garrett Lenihan <garrettleihan@gmail.com>
2025-03-15 21:30:10 +0000

    Minor adjustment in server.js to allow render to diploy application

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ const swaggerOptions = {
 const result = dotenv.config();
 if (result.error) {
   console.log(result.error.message);
-  process.exit(1);
+  // process.exit(1);
 }
 
 async function init() {
