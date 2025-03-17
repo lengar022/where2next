ef09c898fc48d0d01caaa9a09c6dab27ce01746a
Garrett Lenihan <garrettleihan@gmail.com>
2025-03-17 18:54:32 +0000

    OpenStreetMap working on category-view...shows all placemarks on map

--- src/models/mongo/seed-data.js
+++ src/models/mongo/seed-data.js
@@ -32,8 +32,15 @@ export const seedData = {
     placemark1 : {
       name: "Bother Glas",
       description: "Walkway along the river Clyda in Mourneabbey",
-      latitude: 12.22,
-      longitude: 12.44,
+      latitude: 52.069913,
+      longitude: -8.638435,
+      categoryid: "->categories.greenway"
+    },
+    placemark2 : {
+      name: "Waterford Greenway",
+      description: "Former railway track in Waterford now used for cycling and hiking",
+      latitude: 52.26991,
+      longitude: -7.138,
       categoryid: "->categories.greenway"
     },
   }
